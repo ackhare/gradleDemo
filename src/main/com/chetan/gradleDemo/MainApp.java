686dad14f1b25ff6e57df89b6fd68b8beb67e930
chetan <bairagisirfire@gmail.com>
2016-11-05 19:06:23 +0530

    added sourcets and now a jar file and a class file is generated

--- src/main/com/chetan/gradleDemo/MainApp.java
+++ src/main/com/chetan/gradleDemo/MainApp.java
@@ -4,9 +4,9 @@ import java.util.Arrays;
 
 class MainApp{
 
-public static void main(String.. args)
+public static void main(String[] args)
 {
-System.out.println(Arrays.toString(args))
+System.out.println(Arrays.toString(args));
 }
 }
 
