cb137ff0f204dbc4a3bc321fe06d70abfbfeb6bb
chetan <bairagisirfire@gmail.com>
2016-11-06 08:30:59 +0530

    added custom config successfully

--- src/main/com/chetan/gradleDemo/MainApp.java
+++ src/main/com/chetan/gradleDemo/MainApp.java
@@ -1,13 +1,16 @@
 package com.chetan.gradleDemo;
 
+import org.apache.commons.lang3.StringUtils;
+
 import java.util.Arrays;
 
-class MainApp{
+class MainApp {
 
-public static void main(String[] args)
-{
-System.out.println(Arrays.toString(args));
-}
+    public static void main(String[] args) {
+
+
+        System.out.println(StringUtils.swapCase(Arrays.toString(args)));
+    }
 }
 
 
